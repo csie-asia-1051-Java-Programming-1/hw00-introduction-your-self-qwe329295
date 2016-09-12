7baee455b153c0f3168311cb82d785e4dba88caa
qwe329295 <105021028@live.asia.edu.tw>
2016-09-12 15:48:05 +0800

    hw00-1 homework submit

---
+++ hw00-1/src/homework.java
@@ -0,0 +1,13 @@
+
+public class homework {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("我叫蕭佑誠") ;
+  System.out.println("資工數位1A") ;
+	}
+
+}
